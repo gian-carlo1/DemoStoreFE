feb0b15d695bab97528faca78af5c5c1cedd8ab0
Gian Carlo <g.guidotti2000@gmail.com>
2024-11-22 19:29:24 +0100

    paginator + view change button

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,16 +2,16 @@ import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { Store } from '../../models/store';
 import { StoreService } from '../../services/store.service';
 import { StoreCardComponent } from '../../components/store-card/store-card.component';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  imports: [StoreCardComponent, MatPaginator],
-  host: { class: 'dashboard' },
-  encapsulation: ViewEncapsulation.None,
+  imports: [StoreCardComponent, MatPaginator, MatButtonToggleModule, MatIconModule],
 })
 export class DashboardComponent implements OnInit {
   storeService = inject(StoreService);
@@ -20,22 +20,41 @@ export class DashboardComponent implements OnInit {
   stores: Store[] = [];
   storesPaginated: Store[] = []
 
-  pageSize = 5;
-  pageIndex = 1;
   length = 0;
+
+  viewGrid = false
 
   async ngOnInit() {
     this.stores = await this.storeService.getStores();
+
+
     for (let index = 0; index < 20; index++) {
-      this.stores.push(this.stores[0]);
+      const testValue: Store = {
+        ...this.stores[0],
+        data: {
+          ...this.stores[0].data,
+          name: `testValue${index}`
+        }
+      }
+      this.stores.push(testValue);
     }
+
+
     this.length = this.stores.length;
-    this.storesPaginated = this.stores.slice(0, this.pageSize)
+    this.storesPaginated = this.stores.slice(0, 3)
   }
 
   onCardNavigate(store: Store) {
     this.router.navigate([`/store/${store.id}`]);
   }
 
-  handlePageEvent(event: any) {}
+  handlePageEvent(event: any) {
+    const start = event.pageIndex * event.pageSize
+    const end = (event.pageIndex + 1) * event.pageSize
+    this.storesPaginated = this.stores.slice(start, end)
+  }
+
+  onViewChange(event: any) {
+    this.viewGrid = event.value
+  }
 }
