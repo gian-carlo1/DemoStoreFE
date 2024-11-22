d522bcad25ce5abd70a9d73ec1fa3ecb78a30cd6
Gian Carlo <g.guidotti2000@gmail.com>
2024-11-22 18:08:20 +0100

    css -> scss

--- src/app/components/store-card/store-card.component.ts
+++ src/app/components/store-card/store-card.component.ts
@@ -6,7 +6,7 @@ import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-store-card',
   templateUrl: './store-card.component.html',
-  styleUrl: './store-card.component.css',
+  styleUrl: './store-card.component.scss',
   imports: [MatCardModule, MatIcon],
 })
 export class StoreCardComponent {
